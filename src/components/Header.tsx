3e89c1ce5878fdffe73b35de4f2b54317393cb44
Terper <jann.totterman@gmail.com>
2025-09-15 18:10:22 +0300

    new note button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,50 @@
-import React, { useState } from 'react'
-import { Select, SelectContent, SelectItem, SelectTrigger } from './ui/select'
-import { SelectGroup, SelectValue } from '@radix-ui/react-select'
+import React, { useState } from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger } from "./ui/select";
+import { SelectGroup, SelectValue } from "@radix-ui/react-select";
+import { Button } from "./ui/button";
 
-type Props = {}
+type Props = {};
 
 const boards = [
-    { id: 1, name: 'Board 1' },
-    { id: 2, name: 'Board 2' },
-    { id: 3, name: 'Board 3' },
-]
+  { id: 1, name: "Board 1" },
+  { id: 2, name: "Board 2" },
+  { id: 3, name: "Board 3" },
+];
 
 const Header = (props: Props) => {
-    const [selectedBoard, setSelectedBoard] = useState(boards[0])
+  const [selectedBoard, setSelectedBoard] = useState(boards[0]);
+
+  const generateNewNote = () => {
+    console.log("New note generated");
+  };
 
   return (
-    <header>
-        <Select
-            value={selectedBoard.name}
-            onValueChange={(value) => {
-                const board = boards.find((b) => b.name === value)
-                if (board) {
-                    setSelectedBoard(board)
-                }
-            }}
-        >
-            <SelectTrigger className="w-[180px]">
-                <SelectValue />
-            </SelectTrigger>
-            <SelectContent>
-                <SelectGroup>
-                    {boards.map((board) => (
-                        <SelectItem key={board.id} value={board.name}>
-                            {board.name}
-                        </SelectItem>
-                    ))}
-                </SelectGroup>
-            </SelectContent>
-        </Select>
+    <header className="flex items-center justify-between p-2 border-b gap-2">
+      <Select
+        value={selectedBoard.name}
+        onValueChange={(value) => {
+          const board = boards.find((b) => b.name === value);
+          if (board) {
+            setSelectedBoard(board);
+          }
+        }}
+      >
+        <SelectTrigger className="flex-1">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {boards.map((board) => (
+              <SelectItem key={board.id} value={board.name}>
+                {board.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Button onClick={() => generateNewNote()}>New note</Button>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
