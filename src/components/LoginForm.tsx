d625747efc74551de01a21232e4aa71b59688a8b
Terper <jann.totterman@gmail.com>
2025-09-27 19:28:14 +0300

    login

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from "@/AuthProvider";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "./ui/button";
@@ -29,12 +30,41 @@ const LoginForm = () => {
   });
 
   const onSubmit = (data: z.infer<typeof formSchema>) => {
-    console.log("Login data:", data);
-    addToken("token");
+    mutation.mutate(data);
   };
+
+  const mutation = useMutation({
+    mutationFn: async (data: z.infer<typeof formSchema>) => {
+      const response = await fetch(
+        `${import.meta.env.VITE_AUTH_API_URL}/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: data.username,
+            password: data.password,
+          }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error();
+      }
+      return response.json();
+    },
+    onSuccess: (data) => {
+      addToken(data.token);
+    },
+  });
 
   return (
     <Form {...form}>
+      {mutation.isError && (
+        <div className="text-red-600 text-center mb-2 font-semibold">
+          Could not login.
+        </div>
+      )}
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
         <FormField
           control={form.control}
@@ -62,7 +92,11 @@ const LoginForm = () => {
             </FormItem>
           )}
         />
-        <Button type="submit" className="w-full cursor-pointer">
+        <Button
+          type="submit"
+          className="w-full cursor-pointer"
+          disabled={mutation.isPending}
+        >
           Login
         </Button>
       </form>
